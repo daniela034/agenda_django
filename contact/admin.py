ae4642810dd885aad004f5c56813753b36d88dee
Daniela Fernandes <danielaecfernandes@hotmail.com>
2023-12-14 15:55:46 +0000

    models, admin, settings, views

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+#from .models import Contact -> uma forma de importar
+from contact import models 
 # Register your models here.
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin): 
+    list_display = ('id','first_name', 'last_name', 'phone','created_date',) 
+    ordering = '-id', 
+    list_filter = 'created_date',
+    search_fields = 'id', 'first_name', 'last_name' 
+    list_per_page = 10
+    list_max_show_all = 200
+    list_editable = 'first_name', 'last_name', # para conseguirmos editar sem ir ao contacto
+    list_display_links = 'id', 'phone', # onde vamos ter o link para o contacto
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+# id (primary key) -> gerado automaticamente pelo django
+# first_name(string), last_name(string), phone(string)
+# email(email), created_date(date), description(text)
+# os que não têm blank são obrigatorios e na parte de admin do django aparecem a negrito para serem preenchidos 
+
+# depois
+# category(foreign key), show(boolean), owner(foreign key), 
+# picture(image)
+# foreign key -> vai "apontar" para outra tabela
+
+# estamos a criar a tabela contacto
+class Contact(models.Model): 
+    # criar campos na tabela 
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True) # blank -> para ser opcional
+    created_date = models.DateTimeField(default=timezone.now) # não vamos querer que seja regitado pela pessoa
+    description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+    
+# vamos criar a foreign key 
+
+    
