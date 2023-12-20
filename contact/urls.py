49e1faecf1409f1a303d03eaa0ecd5f2e2ee0018
Daniela Fernandes <danielaecfernandes@hotmail.com>
2023-12-20 17:44:02 +0000

    delete and update buttons and actions

--- contact/urls.py
+++ contact/urls.py
@@ -11,6 +11,9 @@ urlpatterns = [
     # quando são paginas dinâmicas por exemplo para o contacto 
     # usamos o CRUD
     path('contact/<int:contact_id>/', views.contact, name='contact'), # leitura
-    path('contact/create/', views.create, name='create')
+    path('contact/create/', views.create, name='create'), 
+    path('contact/<int:contact_id>/update/', views.update, name='update'), # update do contacto 
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'), # update do contacto 
+
     
 ]
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,25 +1,27 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from contact.models import Contact
 from contact.forms import ContactForm
+from django.urls import reverse
 # Create your views here.
 
 
 def create(request):
+    form_action = reverse('contact:create')
     if request.method == 'POST': 
         form = ContactForm(data=request.POST) 
-        context = {'form': form }
+        context = {'form': form , 'form_action': form_action}
         
         # validação se o form é valido para depois conseguirmos guardar os dados adicionados na bd 
         #   vai verificar se o formulário não tem nenhum dos erros que criamos 
         if form.is_valid(): 
             print ('form is valid')
-            form.save() # salvamos assim na base de dados 
+            contact = form.save() # salvamos assim na base de dados 
             # podemos fazer também assim se quisermos alterar alguma coisa 
             # contact = form.save(commit=False)
             # contact.show = False 
             # contact.save()
             #para sairmos da pagina da criação do contacto com os dados lá, voltamos para a pagina de criação mas sem os dados 
-            return redirect('contact:create')
+            return redirect('contact:update', contact_id=contact.pk )
             
         return render(request, 
                   'contact/create.html',
@@ -27,8 +29,44 @@ def create(request):
                   )
         
         
-    context = {'form': ContactForm()}
+    context = {'form': ContactForm(), 'form_action': form_action}
     return render(request, 
                   'contact/create.html',
                   context
                   )
+    
+
+def update(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    form_action = reverse('contact:update', args=(contact_id,))
+    
+    if request.method == 'POST': 
+        form = ContactForm(data=request.POST, instance=contact) 
+        context = {'form': form , 'form_action': form_action}
+        
+        if form.is_valid(): 
+            print ('form is valid')
+            contact = form.save() # salvamos assim na base de dados 
+            return redirect('contact:update', contact_id=contact.pk )
+            
+        return render(request, 
+                  'contact/create.html',
+                  context
+                  )
+        
+        
+    context = {'form': ContactForm(instance=contact), 'form_action' : form_action}
+    return render(request, 
+                  'contact/create.html',
+                  context
+                  )
+    
+def delete(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    
+    confirmation = request.POST.get('confirmation', 'no')
+    if confirmation == 'yes': 
+        contact.delete()
+        return redirect('contact:index')
+    
+    return render(request, 'contact/contact.html', context={'contact':contact, 'confirmation' : confirmation})
